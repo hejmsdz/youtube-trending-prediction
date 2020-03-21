db88c7a883b56bf92303be62313f05e4356562b1
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2020-03-21 12:24:17 +0100

    examine categories

--- notebooks/loaders.py
+++ notebooks/loaders.py
@@ -1,3 +1,4 @@
+import json
 import os.path
 import pandas as pd
 
@@ -5,7 +6,18 @@ data_dir = '../youtube_data'
 
 def load_csv(filename):
     path = os.path.join(data_dir, filename)
-    return pd.read_csv(path, sep=';', encoding='macroman')
+    return pd.read_csv(path, sep=';', encoding='macroman', dtype={'category_id': 'category'})
+
+def load_categories():
+    gb_path = os.path.join(data_dir, 'GB_category_id.json')
+    us_path = os.path.join(data_dir, 'US_category_id.json')
+    categories = {}
+    for path in [gb_path, us_path]:
+        with open(path) as f:
+            data = json.load(f)
+        for category in data['items']:
+            categories[category['id']] = category['snippet']['title']
+    return categories
 
 def load_gb_videos():
     return load_csv('GB_videos_5p.csv')

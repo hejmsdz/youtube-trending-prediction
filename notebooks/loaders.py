f31c31e7eacdc384ec7937265f85015a2585a1f0
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2020-03-28 17:39:44 +0100

    remember broken IDs

--- notebooks/loaders.py
+++ notebooks/loaders.py
@@ -46,21 +46,43 @@ def load_all_videos():
     vids.to_csv(path)
     return vids
 
+def load_broken_ids():
+    path = os.path.join(data_dir, 'broken_ids.txt')
+    broken_ids = set()
+    try:
+        with open(path) as f:
+            for line in f:
+                broken_ids.add(line.strip())
+    except IOError:
+        pass
+    return broken_ids
+
+def save_broken_ids(broken_ids):
+    path = os.path.join(data_dir, 'broken_ids.txt')
+    with open(path, 'w') as f:
+        for l in broken_ids:
+            f.write(f"{l}\n")
+
 def load_thumbnails(vids):
     path = os.path.join(data_dir, 'thumbnails')
     Path(path).mkdir(parents=True, exist_ok=True)
 
     unique_vids = vids.groupby('video_id').first()
     num_images = 0
+    broken_ids = load_broken_ids()
     session = requests.Session()
     for video_id, row in tqdm.tqdm(unique_vids.iterrows(), total=len(unique_vids)):
+            if video_id in broken_ids:
+                continue
             thumbnail_path = os.path.join(path, video_id + ".jpg")
             if os.path.exists(thumbnail_path):
                 continue
             response = session.get(row.thumbnail_link)
             if response.status_code == 404:
+                broken_ids.add(video_id)
                 continue
             with open(thumbnail_path, 'wb') as target:
                 target.write(response.content)
             num_images += 1
+    save_broken_ids(broken_ids)
     return num_images

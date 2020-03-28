caa594527078bda3f71b7b09dde22ef8b1fb4fc5
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2020-03-28 23:11:31 +0100

    extract color palette

---
+++ notebooks/imgutils.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+# https://gist.github.com/PolarNick239/691387158ff1c41ad73c
+def rgb_to_hsv(rgb):
+    input_shape = rgb.shape
+    rgb = rgb.reshape(-1, 3)
+    r, g, b = rgb[:, 0], rgb[:, 1], rgb[:, 2]
+
+    maxc = np.maximum(np.maximum(r, g), b)
+    minc = np.minimum(np.minimum(r, g), b)
+    v = maxc
+
+    deltac = maxc - minc
+    s = deltac / maxc
+    deltac[deltac == 0] = 1
+    rc = (maxc - r) / deltac
+    gc = (maxc - g) / deltac
+    bc = (maxc - b) / deltac
+
+    h = 4.0 + gc - rc
+    h[g == maxc] = 2.0 + rc[g == maxc] - bc[g == maxc]
+    h[r == maxc] = bc[r == maxc] - gc[r == maxc]
+    h[minc == maxc] = 0.0
+
+    h = (h / 6.0) % 1.0
+    res = np.dstack([h, s, v])
+    return res.reshape(input_shape)
